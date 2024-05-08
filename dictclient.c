7694142cac69cffc52aff7427c159d52ca75dafa
26jm35 <jm35@williams.edu>
2024-05-08 15:19:22 -0400

    Finished lab 7

--- dictclient.c
+++ dictclient.c
@@ -169,17 +169,29 @@ int setup_socket() {
   struct addrinfo *servinfo;  // will point to the getaddrinfo results 
   char *hostname, *portno;
 
+  hostname = "www.dict.org";
+  portno = "2628";
+
   /* set up struct for getaddrinfo */
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;        /* use IPv4  */
+  hints.ai_socktype = SOCK_STREAM;  /* use TCP */
+  hints.ai_flags = AI_PASSIVE;      /* fill in my IP addr */
 
   /* populate servinfo with server name and port */
-
+  status = getaddrinfo(hostname, portno, &hints, &servinfo);
   /* make a socket */
-
+  sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+  if (sockfd < 0) {
+    error("ERROR creating socket.");
+  }
   /* connect! */
-
+  if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+    error("ERROR connecting.");
+  }
   /* freeaddrinfo and return sockfd */
-
-  return 0;
+  freeaddrinfo(servinfo);
+  return sockfd;
 }
 
 /*
@@ -200,11 +212,14 @@ void part_a() {
   printf("Enter a word: ");
 
   /* read from stdin */
-
+  res = fgets(input, sizeof(input), stdin);
+  if (res == NULL) {
+    error("ERROR reading from stdin.");
+  }
   /* create socket, connect to server, get fd */
-
+  sockfd = setup_socket();
   /* read from socket to find synonym */
-
+  find_synonym(input, result, sockfd);
   printf("Synonym: %s\n", result);
 }
 
@@ -226,9 +241,9 @@ void *thread_func_single_word (void *argsp) {
   word = (char *) argsp;  
 
   /* create socket, connect to server */
-
+  sockfd = setup_socket();
   /* read from socket to find synonym */
-
+  find_synonym(word, result, sockfd);
   /* copy synonym (aka result) back to word buffer to avoid memory errors */
   memset(word, 0, BUFSIZE);
   memcpy(word, result, strlen(result)); 
@@ -239,11 +254,11 @@ int main(int argc, char **argv) {
   /* Part A */
   // comment this part out after you get Part A working
   // NOTE: make sure this is commented out before submitting to autograder!
-  part_a();
+  //part_a();
 
   /* Part B */
   // uncomment when you're ready to test thread code 
-  //part_b();
+  part_b();
 
   return 0;
 }
